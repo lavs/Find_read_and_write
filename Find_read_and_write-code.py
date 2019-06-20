ac0a9eceac188c24a9b944ceb5c0776af8598a4e
lavs <47961813+lavs@users.noreply.github.com>
2019-06-20 18:31:33 +0700

    Update Find_read_and_write-code.py
    Пути до исходных и итогового файлов формируются относительно расположения Find_read_and_write-code.py
    Добавлена возможность задавать значение сквозного номера (уникального ID) в итоговом файле.

--- Find_read_and_write-code.py
+++ Find_read_and_write-code.py
@@ -7,14 +7,14 @@ def write_file(work_file, file_name, numPP):
             j += 1
         else:
             list = []
-            list.append(numPP)
-            list.append(j - 2)
-            list.append(file_name)
+            list.append(numPP)      #сквозной номер
+            list.append(j - 2)      #номер внутри файла
+            list.append(file_name)  #имя исходного файла
             k = i.split()
             for g in range(len(k)):
                 if g > 0:
                     x = '{:.6f}'.format(float(k[g]))
-                    print('Значения: ', x)
+                    print('Значения x, y, z: ', x)
                     list.append(x)
                 else:
                     list.append(k[g])
@@ -26,23 +26,29 @@ def write_file(work_file, file_name, numPP):
             numPP += 1
             j += 1
 
-    work_file.close()   #закрытие рабочего файла
+    work_file.close()   #закрытие рабочего файла с исходными значениями
     return numPP
 
-final_file = open(r"Structures\structures_all.txt", "w")
-numPP = 1
+group = int(input('Введите номер группы файлов: '))          #номер группы файлов
+path_name = str(input('Введите имя папки с исходными файлами: '))
+print(path_name)
 
-files = [f for f in os.listdir('Structures') if f.endswith('.xyz')] #общий список рабочих файлов
+numPP = group * 10000 + 1          #сквозной номер, уникальный ID
+
+files = [f for f in os.listdir(path_name) if f.endswith('.xyz')] #общий список рабочих файлов
+os.chdir(path_name)  #переходим в папку с исходными файлами
+print('путь', os.getcwd())
+
+final_file = open(path_name + "_all.csv", "w")    #открываем файл для записи итогов
 
 for i in range(len(files)):
     print('имя файла', i + 1, files[i]) #рабочий файл
 
-    os.chdir(r"D:\tmp\projects\Structures")  #переходим в папку с исходными файлами
-    print('путь', os.getcwd())
-
     file = files[i]
-    file_name = file[:(len(file) - 4)]
+    file_name = str(group) + file[:(len(file) - 4)]
     work_file = open(file)
     numPP = write_file(work_file, file_name, numPP)
 
 final_file.close()  #закрытие итогового файла
+
+input('Для выхода нажмите "Enter"')
